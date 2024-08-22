b03e164ce958702d8a256be545c8bd6dc4ed8ef8
Huy Hoang <huyhoang.ng203@gmail.com>
2024-08-22 10:33:07 +0700

    Challenge 04 code + minh chung

--- sourcCode/bai01.js
+++ sourcCode/bai01.js
@@ -31,7 +31,7 @@ let johnBMI = johnMass / johnHeight ** 2;
 // whether Mark has a higher BMI than John.
 let markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
-
+console.log("-------------------");
 
 
 // Chalelnge 2
@@ -46,6 +46,7 @@ if (markHigherBMI) {
 } else {
     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
 }
+console.log("-------------------");
 
 //Challenge 3
 // There are two gymnastics teams, Dolphins and Koalas. They compete against each
@@ -124,4 +125,26 @@ else if (dolphinsAverage2 === koalasAverage2 && dolphinsAverage2 >= 100 && koala
 else {
     console.log('No one wins the trophy 😭');
 }
+console.log("-------------------");
+
+// Challenge 4
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+// 300. If the value is different, the tip is 20%.
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+// this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+// start with an if/else statement, and then try to convert it to a ternary
+// operator!)
+// 2. Print a string to the console containing the bill value, the tip, and the final value
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+// 316.25”
+
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+// this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+// start with an if/else statement, and then try to convert it to a ternary operator!)
+let bill = 275;
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+console.log("-------------------");
 
