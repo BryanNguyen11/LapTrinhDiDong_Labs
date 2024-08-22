e17f99ac4e46338e58c3ba0424b89a1db09ec3e1
Huy Hoang <huyhoang.ng203@gmail.com>
2024-08-22 10:16:50 +0700

    Challenge 02 code + same img with 01

--- sourcCode/bai01.js
+++ sourcCode/bai01.js
@@ -32,3 +32,18 @@ let johnBMI = johnMass / johnHeight ** 2;
 let markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
 
+
+
+// Chalelnge 2
+// Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
+
